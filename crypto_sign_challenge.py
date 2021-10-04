fd67422d8dfe55ec752de84332549e29f99507a6
Kumar Nalinaksh <kumarnalinaksh21@gmail.com>
2021-10-04 17:49:19 +0530

    increased modularity, added test file

--- crypto_sign_challenge.py
+++ crypto_sign_challenge.py
@@ -3,7 +3,14 @@ import os
 import platform
 import base64
 import json
+import logging
 from OpenSSL import crypto
+
+#---------------------- Initialising Logger------------------------#
+logging.basicConfig(filename='crypto-sign-challenge.log', filemode='a', format='%(asctime)s - %(name)s - %(message)s')
+logger=logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
 
 #---------------------- Initialising Global Variables------------------------#
 message = "" #initialising empty message
@@ -19,25 +26,29 @@ flag = False #set global flag as false
 numberOfArguments = len(sys.argv) #total arguments passed to the script
 
 
-#---------------------- Funtion parses arguments, checks min and max character limits and forms message------------------------#
-def CheckInputAndConsolidateMessage(): 
-    global numberOfArguments, message, count 
-
+#---------------------- Funtion generates message from Command Line arguments------------------------#
+def ConsolidateMessage():
+    global message, numberOfArguments
     for i in range(1, numberOfArguments): #Forming message from the argument
         message = message + sys.argv[i] + " "
+    logger.debug("Constructing the message from parsed arguments.")
+
+
+#---------------------- Funtion parses arguments, checks min and max character limits and forms message------------------------#
+def Check_Input(): 
+    global numberOfArguments, message, count, flag
 
     for i in message: #counting number of characters in the message
         count = count + 1
     count = count - 1 #removing the count for the space character at the end of the message
 
-    if(count<=1): #giving alert if number of characters are insufficient or more than required.
-        print("The message must be more than 1 character and less than 250 characters.")
-        return False 
-    elif(count>250):
-        print("The message must be 250 characters or less.")
-        return False
+    if(count<=1 or count>250): #giving alert if number of characters are insufficient or more than required.
+        logger.debug("The message must be more than 1 character and less than 250 characters.")
+        flag = False
+        logger.debug("Script execution is unsuccessfull!") 
     else: 
-        return True
+        flag = True
+        logger.debug("The message is within acceptable parameters")
 
 #---------------------- Funtion generates Private and Public key pairs------------------------#        
 def CreateKeyPair():
@@ -46,26 +57,33 @@ def CreateKeyPair():
     pkey.generate_key(Type_Key, bits) #generating key pair
     private_key = crypto.dump_privatekey(crypto.FILETYPE_PEM , pkey , None) #saving private key contents in byte form 
     public_key = crypto.dump_publickey(crypto.FILETYPE_PEM , pkey) #saving public key contents in byte form 
+    logger.debug("Key pair has been generated")
 
 #---------------------- Funtion reads Private and Public keys from their respective PEM files------------------------#   
 def ReadKeyPairFromPEMFile(PrivFilePath, PubFilepath):
     global private_key, public_key
     with open(PrivFilePath, 'rb') as PrivFile:
         private_key = crypto.load_privatekey(crypto.FILETYPE_PEM, PrivFile.read(), None) #loading private key from PEM file in pkey() object form
+        logger.debug("Private key has been loaded from the PEM file.")    
     with open(PubFilepath, 'rb') as PubFile:
         public_key = PubFile.read().decode('utf-8') #assigning Base64 encoded string (PEM format) of the public key from PEM file
+        logger.debug("Public key has been loaded from the PEM file and decoded to Base64.")
             
 #---------------------- Funtion writes Private and Public keys into their respective PEM files------------------------# 
 def WriteKeyPairToPEMFile(PrivFilePath, PubFilepath):
     global private_key, public_key
     with open(PrivFilePath, 'wb') as PrivFile:
         PrivFile.write(private_key) #writing private key to PEM file 
+        logger.debug("Private key has been written to the PEM file.")
     with open(PrivFilePath, 'rb') as PrivFile:
         private_key = crypto.load_privatekey(crypto.FILETYPE_PEM, PrivFile.read(), None) #reassiging private key content to variable in pkey() object form 
+        logger.debug("Private key has been configured in the form of pkey() object.")
     with open(PubFilepath, 'wb') as PubFile:
         PubFile.write(public_key) #writing public key to PEM file
+        logger.debug("Public key has been written to the PEM file.")
     with open(PubFilepath, 'rb') as PubFile:
         public_key = PubFile.read().decode('utf-8') #assigning Base64 encoded string (PEM format) of the public key from PEM file
+        logger.debug("Public key has been loaded from the PEM file and decoded to Base64.")
 
 #---------------------- Funtion checks the type of operating system on which script is being run------------------------#
 def CheckOperatingSystemType():
@@ -85,6 +103,7 @@ def CheckIfdirectoryExistsAndThenConfigureKeys():
         finalPath = path + subPath #path for Linux default directory
     
     if os.path.exists(finalPath): #Directory for keys exists 
+         logger.debug("Default directory  exists.")
          if CheckOperatingSystemType() == "Windows":
              PubFilepath = finalPath + "\PublicKey.pem" #initialise path for public key in Windows
              PrivFilePath = finalPath + "\PrivateKey.pem" #initialise path for private key in Windows
@@ -93,25 +112,28 @@ def CheckIfdirectoryExistsAndThenConfigureKeys():
              PrivFilePath = finalPath + "/PrivateKey.pem" #initialise path for private key in Linux
 
          if os.path.exists(PubFilepath) and os.path.exists(PrivFilePath): #check if both the keys exist
+             logger.debug("Private and Public keys exist.")
              ReadKeyPairFromPEMFile(PrivFilePath, PubFilepath) #read keys from PEM files and configuring them into variables
 
          else: #if keys don't exist, create them
+             logger.debug("Private and Public keys does not exist.")
              CreateKeyPair() #generating Private and Public keys
              WriteKeyPairToPEMFile(PrivFilePath, PubFilepath) #writing keys into PEM files and configuring them into variables
 
     else: #Directory for keys does not exists 
-         print(finalPath, " does not esist.")
+         logger.debug("Default directory does not esist.")
          try : 
+             logger.debug("Attempting to creating the directory")
              os.mkdir(finalPath)  # trying to create the directory 
          except :
              pass #executing further script in case directory creation encounters an error
          if os.path.exists(finalPath) == True: #checking if directory has been created successfully
-             print("Directory Created")
+             logger.debug("Directory created successfully!")
              CheckIfdirectoryExistsAndThenConfigureKeys() #recursion
          else: 
-             print(finalPath, " could not be created due to access permission issue.")
+             logger.debug("Default directory could not be created due to access permission issue.")
              path = os.getcwd() #initialising current working directory as path
-             print("Instead of ", finalPath, " program will now use current working directory ", path)
+             logger.debug("Instead of default directory program will now use current working directory.")
              if CheckOperatingSystemType() == "Windows":
                  PubFilepath = path + "\PublicKey.pem" #initialise path in current working directory for public key in Windows
                  PrivFilePath = path + "\PrivateKey.pem" #initialise path in current working directory for private key in Windows
@@ -120,9 +142,11 @@ def CheckIfdirectoryExistsAndThenConfigureKeys():
                  PrivFilePath = path + "/PrivateKey.pem" #initialise path in current working directory for private key in Linux
 
              if os.path.exists(PubFilepath) and os.path.exists(PrivFilePath): #check if both the keys exist
+                 logger.debug("Private and Public keys exist.")
                  ReadKeyPairFromPEMFile(PrivFilePath, PubFilepath) #read keys from PEM files and configuring them into variables
 
              else: #if keys don't exist, create them
+                 logger.debug("Private and Public keys does not exist.")
                  CreateKeyPair() #generating Private and Public keys
                  WriteKeyPairToPEMFile(PrivFilePath, PubFilepath) #writing keys into PEM files and configuring them into variables
 
@@ -132,21 +156,29 @@ def SigningTheMessage():
     global signature, private_key, message
     signature = crypto.sign(private_key, message.encode(), "sha256") #forming signature using private key and sha256 digest of message
     signature = base64.encodebytes(signature).decode() #Base64 encoding of the cryptographic signature of the message
+    logger.debug("Signature has been formed and decoded to Base64")
 
     
 #-------------Function generates JSON compliant to the schema defined in README----------------#
 def FormJSON():
     global  resultJSON, message, signature, public_key
     resultJSON = { "message": message, "signature":signature, "pubkey":public_key} #initialising dictionary complaint with JSON
+    logger.debug(resultJSON)
     resultJSON = json.dumps(resultJSON, indent=3) # serialising into JSON
+    logger.debug("JSON has been formed and decoded to Base64")
     print(resultJSON)
+    return True
 
 #-------------Main Function----------------#
 def Main(): 
-    if CheckInputAndConsolidateMessage(): #checking input and consolidating message, if as per policy then program will proceed.
+    logger.debug("Script has been invoked!")
+    ConsolidateMessage() #generates message from Command Line arguments
+    Check_Input() #checking input and consolidating message, if as per policy then program will proceed.
+    if flag == True:
         CheckIfdirectoryExistsAndThenConfigureKeys() #checking if directory exists, else we try to create it. If unsuccessful then we use current working directory and configure the keys
         SigningTheMessage() #generating RFC 4648 compliant Base64 encoded cryptographic signature of the message, calculated using the private key and the SHA256 digest of the message
         FormJSON() #generating JSON compliant to the schema defined in README
+        logger.debug("Script execution has completed successfully!")
 
 
 
--- test.py
+++ test.py
@@ -1,3 +1,31 @@
-import main
+import crypto_sign_challenge
+import unittest
+import argparse
+import os, sys
 
-main.Main()
+class TestCryptoSignChallenge(unittest.TestCase):
+    def test_withMoreThan256Characters(self):
+        IntendedFlag = False
+        argument = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec."
+        crypto_sign_challenge.message = argument
+        crypto_sign_challenge.Check_Input()
+        ObservedOutput = crypto_sign_challenge.flag
+        self.assertEqual(IntendedFlag, ObservedOutput)
+        
+    def test_WithoutArguments(self):
+        IntendedFlag = False
+        crypto_sign_challenge.Main()
+        ObservedOutput = crypto_sign_challenge.flag
+        self.assertEqual(IntendedFlag, ObservedOutput)
+        
+    def test_withNormalArguments(self):
+        argument = "Hello Zindagi"
+        crypto_sign_challenge.message = argument
+        crypto_sign_challenge.CheckIfdirectoryExistsAndThenConfigureKeys()
+        crypto_sign_challenge.SigningTheMessage()
+        self.assertTrue(crypto_sign_challenge.FormJSON())
+
+
+if __name__ == "__main__":
+    unittest.main()
+
